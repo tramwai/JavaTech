7eca6635dfb0ea7f4436e55599f03acdbe7f5f3b
tramwai <MIHAIL.HARITON@GMAIL.COM>
2023-01-09 22:33:50 +0200

    "added tasks"

--- src/homeworks/Homework14.java
+++ src/homeworks/Homework14.java
@@ -60,5 +60,70 @@ Test Data 5: 15
 Expected Result 5: FizzBuzz
      */
 
+// Task3
+    /**
+    Requirement:
+-Create a method called findSumNumbers()
+-This method will take a String argument and it will return an int which
+is the sum of all numbers presented in the String.
 
+NOTE: If there are no numbers represented in the String, return 0.
+
+Test Data 1: “abc$”
+Expected Result 1: 0
+
+Test Data 2: “a1b4c  6#”
+Expected Result 2: 11
+
+Test Data 3: “ab110c045d”
+Expected Result 3: 155
+
+Test Data 4: “525”
+Expected Result 4: 525
+     */
+
+    // Task 4
+    /**
+    Requirement:
+-Create a method called findBiggestNumber()
+-This method will take a String argument and it will return an int which is the number presented in the String.
+
+NOTE: If there are no numbers represented in the String, return 0.
+
+Test Data 1: “abc$”
+Expected Result 1: 0
+
+Test Data 2: “a1b4c  6#”
+Expected Result 2: 6
+
+Test Data 3: “ab110c045d”
+Expected Result 3: 110
+
+Test Data 4: “525”
+Expected Result 4: 525
+
+     */
+
+    // Task 5
+    /**
+    Requirement:
+-Create a method called countSequenceOfCharacters()
+-This method will take a String argument and it will return a String which is the count
+     of repeated characters in a sequence in the String.
+NOTE: If given String is empty, then return empty String.
+NOTE: It is case sensitive!!!
+
+Test Data 1: “”
+Expected Result 1: “”
+
+Test Data 2: “abc”
+Expected Result 2: “1a1b1c”
+
+Test Data 3: “abbcca”
+Expected Result 3: “1a2b2ca”
+
+Test Data 4: “aaAAa”
+Expected Result 4: “2a2A1a”
+
+     */
 }
