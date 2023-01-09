2fd0e2c0c31f622e7251f93c5b3cb8538ac3fc36
tramwai <MIHAIL.HARITON@GMAIL.COM>
2023-01-09 22:29:51 +0200

    initiated homework14

---
+++ src/homeworks/Homework14.java
@@ -0,0 +1,64 @@
+package homeworks;
+
+public class Homework14 {
+
+
+    // Task1
+    /**
+    Requirement:
+-Create a method called fizzBuzz1()
+-This method will take an int argument, and it will print numbers starting from 1 to given argument.
+ BUT, it will print “Fizz” for the numbers divided by 3, “Buzz” for the numbers divided by 5,
+ and “FizzBuzz” for the numbers divided both by 3 and 5.
+
+Test Data 1: 3          Test Data 2: 5          Test Data 3: 18
+Expected Result 1:      Expected Result 2:      Expected Result 3:
+1                       1                       1
+2                       2                       2
+Fizz                    Fizz                    Fizz
+                        4                       4
+                        Buzz                    Buzz
+
+                                                Fizz
+                                                7
+                                                8
+                                                Fizz
+                                                Buzz
+                                                11
+                                                Fizz
+                                                13
+                                                14
+                                                FizzBuzz
+                                                16
+                                                17
+                                                Fizz
+
+     */
+
+
+    // Task 2
+    /**
+    Requirement:
+-Create a method called fizzBuzz2()
+-This method will take an int argument, and it will return a String. BUT it will return “Fizz”
+ if the given number is divided by 3, “Buzz” if the number is divided by 5, and “FizzBuzz”
+ if the number is divided both by 3 and 5. Otherwise, it will return number itself.
+
+Test Data 1: 0
+Expected Result 1: FizzBuzz
+
+Test Data 2: 1
+Expected Result 2: 1
+
+Test Data 3: 3
+Expected Result 3: Fizz
+
+Test Data 4: 5
+Expected Result 4: Buzz
+
+Test Data 5: 15
+Expected Result 5: FizzBuzz
+     */
+
+
+}
